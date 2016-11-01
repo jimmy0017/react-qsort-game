82cb0689b3b3bcf55ba09910bab16ad58d3c5d2a
Jimmy Lin <jimmy0017@gmail.com>
2016-11-01 06:31:26 -0400

    change card.js style

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -3,14 +3,35 @@ import React from "react";
 export default class Card extends React.Component {
   render() {
     const { title,statement } = this.props;
+    const squareStyle = {
+      float:'left',
+      position: 'relative',
+      width: '9%',
+      paddingBottom : '9%', /* = width for a 1:1 aspect ratio */
+      margin :'.3%',
+      backgroundColor:'#FFFFFF',
+      overflow :'hidden',
+      borderStyle: 'solid',
+    };
+    const contentStyle = {
+      position:'absolute',
+      height: '90%', /* = 100% - 2*5% padding */
+      width: '90%', /* = 100% - 2*5% padding */
+      padding: '5%',
+    };
+    const textStyle = {
+      fontSize: '9px'
+    };
+    const textTitleStyle = {
+      fontSize: '12px'
+    };
 
     return (
-      <div class="card well text-center col-md-2">
-        <div class="card-block">
-          <h4 class="card-title text-center">{this.props.title}</h4>
-          <p class="card-text">{this.props.statement}</p>
-          {/* <a href="#" class="btn btn-primary">Zoom in (later)</a> */}
-        </div>
+      <div class="square" style={squareStyle}>
+          <div class="content" style={contentStyle}>
+              <label style={textTitleStyle}>{this.props.title}</label>
+              <p style={textStyle}>{this.props.statement}</p>
+          </div>
       </div>
     );
   }
