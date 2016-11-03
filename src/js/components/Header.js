db8ab64ee4cdfb83d441e55705af728d78aaf808
Jimmy Lin <jimmy0017@gmail.com>
2016-11-02 22:06:09 -0400

    remove unused in header.js

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,12 +1,6 @@
 import React from "react";
 
-import Title from "./Header/Title";
-
 export default class Header extends React.Component {
-  handleChange(e) {
-    const title = e.target.value;
-    this.props.changeTitle(title);
-  }
 
   render() {
     return (
